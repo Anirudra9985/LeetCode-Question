5ea4fef42cc041399648fe6efa2b4d4cfe85c363
Anirudra Singh Chauhan <anirudra.2327cs1239@kiet.edu>
2026-08-22 22:05:37 +0530

    Time: 0 ms (100%), Space: 7.8 MB (68.55%) - LeetHub

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     bool checkDivisibility(int n) {
-        int original = n;
-        int sum = 0;
-        int product = 1;
+        // int original = n;
+        // int sum = 0;
+        // int product = 1;
 
-        while (n > 0) {
-            int digit = n % 10;
+        // while (n > 0) {
+        //     int digit = n % 10;
 
-            sum += digit;
-            product *= digit;
+        //     sum += digit;
+        //     product *= digit;
 
-            n /= 10;
+        //     n /= 10;
+        // }
+
+        // int divisor = sum + product;
+
+        // return original % divisor == 0;
+
+        int s=0,p=1;
+        for(int x=n;x>0;x/=10){
+             const int r = x%10;
+             s+=r;
+             p*=r;
         }
-
-        int divisor = sum + product;
-
-        return original % divisor == 0;
+        return n%(s+p)==0;
     }
 };
