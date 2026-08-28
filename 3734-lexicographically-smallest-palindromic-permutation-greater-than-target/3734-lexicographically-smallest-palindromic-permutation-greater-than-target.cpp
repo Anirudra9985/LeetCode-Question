d92268a986335b3da75740e5abc19396ed637e15
Anirudra Singh Chauhan <anirudra.2327cs1239@kiet.edu>
2026-08-29 00:57:32 +0530

    Time: 0 ms (100%), Space: 10.1 MB (98.46%) - LeetHub

--- 3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
+++ 3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
@@ -1,104 +1,72 @@
-int8_t dp[151];
 class Solution {
 public:
-    array<int, 26> freq{};
-    int n, half;
-    int oddIdx=-1;
-    bool revLeftIsGrR(const string& t){
-        for(int i=0; i<half; i++){
-            if (t[half-1-i]>t[half+(n&1)+i]) return 1;
-            if (t[half-1-i]<t[half+(n&1)+i]) return 0;
+    string lexPalindromicPermutation(string s, string target) {
+        int n=s.size();
+        vector<int>hsh(26,0);
+        for(int i=0;i<n;i++){
+            hsh[s[i]-'a']++;
         }
-        return 0;
-    }
-  
-    bool canPlace(int i, const string& target, unsigned hasC) {
-        if (dp[i]!=-1) return dp[i];
-        const char c=target[i];
-        if (freq[c-'a']==0) return 0;
-
-      
-        unsigned hasC1=hasC;
-        if (--freq[c-'a']==0) hasC1 &= ~(1<<(c-'a'));
-        
-        bool ans=0;
-        if (i==half-1) {
-            if (n&1) {
-                if ('a'+oddIdx!=target[half]) 
-                    ans=('a'+oddIdx > target[half]);
-                else ans=revLeftIsGrR(target);
-            } 
-            else ans=revLeftIsGrR(target);
-        } 
-        else {
-            int nxt=target[i+1]-'a';
-            if (hasC1>>(nxt+1))
-                ans=1;
-            else if (!((hasC1>>nxt) & 1)) 
-                ans=0;
-            else 
-                ans=canPlace(i+1, target, hasC1);
-        }
-       
-        freq[c-'a']++;
-        return dp[i]=ans;
-    }
-    inline string build_palindrome(const string& ans){
-        string pal=ans;
-         if (n&1) pal+='a'+oddIdx;
-        string rev=ans;
-        reverse(rev.begin(), rev.end());
-        pal+=rev;
-        return pal;
-    }
-    string lexPalindromicPermutation(string& s, string& target) {
-        n=s.size(), half=n>>1;
-        memset(dp, -1, half);
-        unsigned par=0, hasC=0;
-        for(char c : s){
-            const int idx=c-'a';
-            freq[idx]++;
-            hasC|=1<<idx;
-            par^=(1<<idx);
-        }
-        if (popcount(par)>1) return "";
-
-        if (n&1) oddIdx=countr_zero(par);
-        for(unsigned mask=hasC; mask; mask&=(mask-1)){
-            int i=countr_zero(mask);
-            freq[i]>>=1;
-          
-            if (freq[i]==0) hasC &=~(1u<<i);
-        }
-
-        string ans="";
-        for (int i=0; i<half; i++) {
-            int t_i=target[i]-'a';
-           
-            if (freq[t_i] > 0 && canPlace(i,target, hasC)) {
-                ans+=target[i];
-                if (--freq[t_i]==0) hasC &= ~(1u<<t_i); 
-            } 
-            else {
-             
-                unsigned higher=hasC>>(t_i+1);
-                if (higher==0) return "";
-                int choice=countr_zero(higher)+t_i+1;
-                if (--freq[choice]==0) hasC &= ~(1u<<choice);
-                ans+='a'+ choice;
-
-               
-                for (int j=i+1; j<half; j++) {
-                    int idx=countr_zero(hasC);
-                    ans+='a'+idx;
-                    if (--freq[idx]==0) hasC &=~(1u<<idx); 
-                }
-                return build_palindrome(ans);
+        string ans=s;
+        int odd=0;
+        for(int i=0;i<26;i++){
+            if(hsh[i]%2!=0){
+                odd++;
+                ans[n/2]=char('a'+i);
             }
+            hsh[i]/=2;
         }
-
-        
-        string pal=build_palindrome(ans);
-        return pal>target ? pal : "";
+        if(odd>1){
+            return "";
+        }
+        int p=0;
+        while(p<n/2 && hsh[target[p]-'a']){
+            hsh[target[p]-'a']--;
+            p++;
+        }
+        int start=min(n-1,p);
+        for(int i=start;i>=0;i--){
+            if(i==(n/2)){
+                for(int j=0;j<n/2;j++){
+                    ans[j] = target[j];
+                    ans[n-1-j] = target[j];
+                }
+                if(ans > target){
+                    return ans;
+                }
+                continue;
+            }
+            int b=target[i]-'a';
+            if(i<p){
+                hsh[b]++;
+            }
+            int idx=-1;
+            for(int c=b+1;c<26;c++){
+                if(hsh[c]){
+                    hsh[c]--;
+                    idx=c;
+                    break;
+                }
+            }
+            if(idx==-1){
+                continue;
+            }
+            for(int j=0;j<i;j++){
+                ans[j]=target[j];
+            }
+            ans[i]=char('a'+idx);
+            int k=i+1;
+            for(int c=0;c<26;c++){
+                while(k<n && hsh[c]){
+                    ans[k]=char(c+'a');
+                    k++;
+                    hsh[c]--;
+                }
+            }
+            for(int j=0;j<n/2;j++){
+                ans[n-1-j]=ans[j];
+            }
+            return ans;
+        }
+        return "";
     }
 };
