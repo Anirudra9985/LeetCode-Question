c6bc5c85407b659185123e5c1bab5830231eb361
Anirudra Singh Chauhan <anirudra.2327cs1239@kiet.edu>
2026-09-02 22:55:38 +0530

    Time: 0 ms (100%), Space: 8.9 MB (92.91%) - LeetHub

--- 0843-guess-the-word/0843-guess-the-word.cpp
+++ 0843-guess-the-word/0843-guess-the-word.cpp
@@ -1,3 +1,55 @@
+// /**
+//  * // This is the Master's API interface.
+//  * // You should not implement it, or speculate about its implementation
+//  * class Master {
+//  *   public:
+//  *     int guess(string word);
+//  * };
+//  */
+// /**
+//  * // This is the Master's API interface.
+//  * // You should not implement it, or speculate about its implementation.
+//  * class Master {
+//  *   public:
+//  *     int guess(string word);
+//  * };
+//  */
+
+// class Solution {
+// public:
+   
+//     int matchCount(const string &a, const string &b) {
+//         int cnt = 0;
+//         for (int i = 0; i < a.size(); i++) {
+//             if (a[i] == b[i]) cnt++;
+//         }
+//         return cnt;
+//     }
+
+//     void findSecretWord(vector<string>& words, Master& master) {
+//         unordered_set<string> candidates(words.begin(), words.end());
+
+//         while (!candidates.empty()) {
+//             // Pick an arbitrary word from the set
+//             string guessWord = *candidates.begin();
+
+            
+//             int matched = master.guess(guessWord);
+
+//             // Filter words that have the same match count
+//             for (auto it = candidates.begin(); it != candidates.end();) {
+//                 if (matchCount(*it, guessWord) != matched)
+//                     it = candidates.erase(it);
+//                 else
+//                     ++it;
+//             }
+
+//             // Remove the used word
+//             candidates.erase(guessWord);
+//         }
+//     }
+// };
+
 /**
  * // This is the Master's API interface.
  * // You should not implement it, or speculate about its implementation
@@ -6,45 +58,25 @@
  *     int guess(string word);
  * };
  */
-/**
- * // This is the Master's API interface.
- * // You should not implement it, or speculate about its implementation.
- * class Master {
- *   public:
- *     int guess(string word);
- * };
- */
-
 class Solution {
 public:
-   
-    int matchCount(const string &a, const string &b) {
+    int getMatch(const string& a, const string& b){
         int cnt = 0;
-        for (int i = 0; i < a.size(); i++) {
-            if (a[i] == b[i]) cnt++;
-        }
-        return cnt;
+        for(int i=0;i<6;i++)
+            if(a[i] == b[i]) cnt++;
+        return cnt;        
     }
-
     void findSecretWord(vector<string>& words, Master& master) {
-        unordered_set<string> candidates(words.begin(), words.end());
-
-        while (!candidates.empty()) {
-            // Pick an arbitrary word from the set
+        unordered_set<string> candidates(words.begin(),words.end());
+        while(!candidates.empty()){
             string guessWord = *candidates.begin();
-
-            
-            int matched = master.guess(guessWord);
-
-            // Filter words that have the same match count
-            for (auto it = candidates.begin(); it != candidates.end();) {
-                if (matchCount(*it, guessWord) != matched)
+            int match = master.guess(guessWord);
+            for(auto it = candidates.begin(); it != candidates.end(); ){
+                if(getMatch(*it,guessWord) != match)
                     it = candidates.erase(it);
                 else
-                    ++it;
+                    it++;
             }
-
-            // Remove the used word
             candidates.erase(guessWord);
         }
     }
